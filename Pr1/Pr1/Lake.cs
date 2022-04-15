42e21ae927fd4e31e679b46bdb33b364fa4ad591
dimkaPro <dimondarkpro11@gmail.com>
2022-04-15 15:20:24 +0300

    Добавили методы класса

--- Pr1/Pr1/Lake.cs
+++ Pr1/Pr1/Lake.cs
@@ -20,7 +20,22 @@ namespace Pr1
             this.Name = name;
             this.Square = Square;
             this.Length = Length;
-        }
 
+        }
+        public void Print()
+        {
+            Console.Write($"Название: {Name}\n" +
+                              $"Площадь бассейна: {Square}\n" +
+                              $"Протяженность: {Length}\n");
+        }
+        public void Input()
+        {
+            Console.Write("Введите название: ");
+            this.Name = Console.ReadLine();
+            Console.Write("Введите площадь бассейна: ");
+            this.Square = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите протяженность: ");
+            this.Length = Convert.ToInt32(Console.ReadLine());
+        }
     }
 }
