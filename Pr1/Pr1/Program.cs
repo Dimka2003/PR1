f2adade34a03ce56a622c8cd11e3fa7e2eed7283
dimkaPro <dimondarkpro11@gmail.com>
2022-04-15 15:21:35 +0300

    Создали метод Main

--- Pr1/Pr1/Program.cs
+++ Pr1/Pr1/Program.cs
@@ -6,7 +6,32 @@ namespace Pr1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Lake a = new Lake();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1 - Добавить реку" + " " +
+                "2 - Вывести реку" + " " + "3 - Выход");
+                int number = Convert.ToInt32(Console.ReadLine());
+                switch (number)
+                {
+                    case 1:
+                        {
+                            a.Input();
+                            break;
+                        }
+                    case 2:
+                        {
+                            a.Print();
+                            break;
+                        }
+                    default:
+                        {
+                            flag = false;
+                            break;
+                        }
+                }
+            }
         }
     }
 }
