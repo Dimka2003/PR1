234b6ba115798572ef6a3fb7042a1549cc40d870
dimkaPro <dimondarkpro11@gmail.com>
2022-04-15 15:19:17 +0300

    Создали конструкторы класса

--- Pr1/Pr1/Lake.cs
+++ Pr1/Pr1/Lake.cs
@@ -9,6 +9,18 @@ namespace Pr1
         public string Name { get; set; }
         public double Square { get; set; }
         public double Length { get; set; }
+        public Lake()
+        {
+            Name = "";
+            Square = 0;
+            Length = 0;
+        }
+        public Lake(string name, double Square, double Length)
+        {
+            this.Name = name;
+            this.Square = Square;
+            this.Length = Length;
+        }
 
     }
 }
